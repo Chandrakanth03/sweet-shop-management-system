0f411208941316a95e4df7462ec99cc82871b973
Kokkonda Chandrakanth Reddy <chandrakanthkokkonda03@gmail.com>
2025-12-13 10:56:05 +0530

    chore: initial project setup

---
+++ Sweet-Shop-Management-System/src/main/java/com/example/Sweet/Shop/Management/System/SweetShopManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.Sweet.Shop.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetShopManagementSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SweetShopManagementSystemApplication.class, args);
+	}
+
+}
